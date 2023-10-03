60290a21246ffd2ed53026ea6def734ab7ee59b8
giriram <giriramdsk@gmail.com>
2023-10-03 22:30:45 +0530

    Boiler plate

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ if (process.env.NODE_ENV !== "production") {
 //         app.listen(process.env.PORT,()=>{console.log(`MVNA running in ${process.env.PORT}`)});
 //     }})
 
-const PORT = 4003; // Use process.env.PORT if available, otherwise, use port 4000
+const PORT = process.env.PORT; // Use process.env.PORT if available, otherwise, use port 4000
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log('process.env.NODE_ENV', process.env.NODE_ENV);
